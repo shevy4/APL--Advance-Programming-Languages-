8915c1cdaaf0d22a823a202e293d0e7d904b4f78
shav <61606455+shevy4@users.noreply.github.com>
2024-06-21 18:39:32 -0500

    Update Syntax.py

--- Syntax.py
+++ Syntax.py
@@ -1,18 +1,39 @@
 from ply import yacc
 from Tokenize import tokens
 
+variables = []
+
 
 # Define grammar rules
 
 def p_expression_lambda(p):
-    """expression : LAMBDA VARIABLE DOT VARIABLE
-                  | LAMBDA VARIABLE DOT LPAREN VARIABLE RPAREN"""
+    """expression : LAMBDA VARIABLE DOT LPAREN expression RPAREN
+                  | LAMBDA VARIABLE DOT expression
+                  | expression expression
+                  | VARIABLE
 
-    # | LAMBDA VARIABLE DOT LPAREN VARIABLE RPAREN"""
-    if len(p) < 6:
-        p[0] = ('lambda', p[2])
-    else:
-        p[0] = ('lambda', p[5])
+                  """
+    if len(p) == 2:
+        print(p[1][0])
+        p[0] = ('variable', p[1])
+
+
+
+
+    elif len(p) == 5 and p[1] == '#':
+        print("PING 5")
+        for _ in range(len(p)):
+            print(p[_])
+        p[0] = ('lambda', p[2], (p[4]))
+        print(p[0])
+        print(variables)
+
+    elif len(p) == 7:
+        print("PING")
+        p[0] = ('lambda', p[2], ('variable', p[4]))
+
+    elif len(p) == 4:
+        p[0] = p[2]
 
 
 def p_error(p):
